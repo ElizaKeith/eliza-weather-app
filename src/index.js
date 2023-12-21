5affc1acf2770ffe7232f2e06e5b8d6192464ab2
ek0179 <Liz.keith56@gmail.com>
2023-12-21 17:03:21 -0500

    added live icon provided by API

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ let temperature= response.data.temperature.current;
   let windSpeedElement= document.querySelector("#wind-speed");
   let timeElement= document.querySelector("#time");
   let date=new Date (response.data.time * 1000);
+let iconElement=document.querySelector("#icon");
 
   cityElement.innerHTML=response.data.city;
   timeElement.innerHTML = formatDate (date);
@@ -14,6 +15,7 @@ let temperature= response.data.temperature.current;
   humidityElement.innerHTML=` ${response.data.temperature.humidity}% `;
   windSpeedElement.innerHTML= ` ${response.data.wind.speed} km/h`;
 temperatureElement.innerHTML=Math.round(temperature);
+iconElement.innerHTML=`<img src="${response.data.condition.icon_url}"  class="weather-app-icon"  />`;
 }
 
 function formatDate(date) {
@@ -22,7 +24,7 @@ let minuets=date.getMinutes();
 let hour=date.getHours();
 let days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let day= days[date.getDay()];
-if (minuets<10) {minuets= `0 ${minuets}`};
+if (minuets<10) {minuets= `0${minuets}`};
 return `${day} ${hour}:${minuets}`
 }
 
@@ -41,4 +43,4 @@ function handleSearch (event){
 
 let searchFormElement = document.querySelector ("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
-searchCity("Richmond");
+searchCity("Midlothian");
