f6a28ae386508ae1801aa5b880b7f528e189bc2f
ek0179 <Liz.keith56@gmail.com>
2023-12-21 15:13:39 -0500

    added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+function refreshWeather(response){
+    let temperatureElement=document.querySelector("#temperature");
+temperature= response.data.temperature.current;
+  let cityElement=document.querySelector("#city");
+  cityElement.innerHTML=response.data.city;
+temperatureElement.innerHTML=Math.round(temperature);
+}
+function searchCity(city){
+let apiKey="o0bta765574648ffe02e8d81539be096";
+apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearch (event){
     event.preventDefault();
     let searchInput= document.querySelector("#search-form-input");
-    let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=searchInput.value;
+  
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector ("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+searchCity("Richmond");
